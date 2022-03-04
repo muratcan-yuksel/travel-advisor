151bf2d60addb41bcabe9f369ba0328cf0374b60
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-04 20:54:45 +0300

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,12 @@ const App = () => {
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
-        <Grid item xs={12} md={4}></Grid>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
       </Grid>
     </>
   );
