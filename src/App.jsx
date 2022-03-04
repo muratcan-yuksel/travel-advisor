b84e8b5ab18ec631abf224c4b78d830259d4e82d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-04 20:39:49 +0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}></Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default App;
